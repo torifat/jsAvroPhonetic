6231733e31003c1c2901b751c57d5ee389971792
Rifat Nabi <to.rifat@gmail.com>
2011-11-11 03:37:02 +0600

    loader added

--- src/avro-bookmarklet-1.0-beta.js
+++ src/avro-bookmarklet-1.0-beta.js
@@ -3,10 +3,16 @@
         avro_js_loader();
         return;
     }
-
+    
     var root = (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]);
     var ns = document.createElementNS && document.documentElement.namespaceURI;
-
+    
+    var loader = ns ? document.createElementNS(ns, 'div') : document.createElement('div');
+    loader.setAttribute('style', 'position:fixed; z-index:1000; top:0; bottom:0; left:0; right:0; background:#000; text-align:center; color:#fff; opacity:0.5;');
+    loader.innerHTML = '<h1 style="margin-top: 30px;">Loading</h1>';
+    loader.setAttribute('id', 'avro_js_loader');
+    (document.getElementsByTagName('body')[0]).appendChild(loader);
+    
     if(typeof jQuery === 'undefined') {
         var script = ns ? document.createElementNS(ns, 'script') : document.createElement('script');
         script.type = 'text/javascript';
@@ -36,6 +42,7 @@
         jQuery('textarea, input[type=text]').live('focus', function() {
             jQuery(this).avro('destroy').avro();
         }).avro('destroy').avro();
+       jQuery('#avro_js_loader').remove();
     }
     
 })();
